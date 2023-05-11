8c1fc61b285ce8ce40df1f277fefa3a27cffd068
ghost-sys-coder <juniorbeast177@gmail.com>
2023-05-11 15:30:09 +0300

    Navbar changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className="logo">
-        <Link href='/'>Beast Headphones</Link>
+        <Link href='/'>Vanox</Link>
       </p>
 
       <button type='button'
